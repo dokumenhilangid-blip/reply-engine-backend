3024eb6565eeaa83858ff8c5d758d7e8526575fc
dokumenhilangid-blip <dokumenhilang.id@gmail.com>
2026-03-01 23:47:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from database import get_conn
+from fastapi import FastAPI
+from database import init_db, get_conn
+
+app = FastAPI()
+
+@app.on_event("startup")
+def startup():
+    init_db()
+
+@app.get("/")
+def root():
+    return {"status": "backend alive"}
+
+@app.get("/test-db")
+def test_db():
+    return {"status": "database ready"}
+
 
 @app.get("/test-write")
 def test_write():
