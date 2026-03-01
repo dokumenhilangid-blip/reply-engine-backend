f48319282da7073398d0d935cfb3eb6079b8ad53
dokumenhilangid-blip <dokumenhilang.id@gmail.com>
2026-03-01 23:50:13 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,23 @@
 import sqlite3
 import os
 
-DB_PATH = os.getenv("DB_PATH", "engine.db")
+DB_PATH = os.getenv("DB_PATH", "/data/engine.db")
+
 
 def init_db():
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS test (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        message TEXT
-    )
+        CREATE TABLE IF NOT EXISTS test_table (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            message TEXT
+        )
     """)
 
     conn.commit()
     conn.close()
+
+
+def get_conn():
+    return sqlite3.connect(DB_PATH)
